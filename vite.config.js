6ef27d1275cbd2c69c17c603d53e974137b7680f
AsepSaniNugraha <asepsaninugraha@gmail.com>
2026-04-21 22:46:23 +0700

    Fixing base for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LanguangeCourse/',
+  base: '/Landing-Page-for-Languange-Course/',
   plugins: [react(), tailwindcss()],
 })
